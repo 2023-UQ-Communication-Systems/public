b2ccaaaf7bf1c63fb4bb6a4514c2dabe175026a3
Konstanty <konstanty@ieee.org>
2022-09-21 17:54:34 +1000

    Fix int2bit
    - had a rows/columns mismatch
    - meant that more than 8 symbols caused issues.

--- python/bitutil.py
+++ python/bitutil.py
@@ -4,7 +4,7 @@ import numpy as np
 def int2bit(x, nbits=8, ismsb=True):
     symbits = np.array(x, dtype=np.uint8).reshape((-1,1))
     bitorder = 'big' if ismsb else 'little'
-    return np.unpackbits(np.array(symbits, dtype=np.uint8), axis=1, bitorder=bitorder)[-nbits:]
+    return np.unpackbits(np.array(symbits, dtype=np.uint8), axis=1, bitorder=bitorder)[:,-nbits:]
 
 
 def bit2int(x, nbits=8, ismsb=True):
