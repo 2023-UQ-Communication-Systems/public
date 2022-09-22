94c50572bdabe6ce17ae957608aee4e230654bde
Konstanty Bialkowski <konstanty@ieee.org>
2022-09-22 10:52:08 +1000

    Improve bit2int
    - use the nbits variable

--- python/bitutil.py
+++ python/bitutil.py
@@ -8,7 +8,18 @@ def int2bit(x, nbits=8, ismsb=True):
 
 
 def bit2int(x, nbits=8, ismsb=True):
+    if nbits > 8:
+        raise ValueError('nbits must be less than or equal to 8')
     bitorder = 'big' if ismsb else 'little'
-    return np.packbits(x, bitorder=bitorder)
+    # pad each row with zeros at the start of each row
+    x = np.reshape(x, (-1, nbits))
+
+    if bitorder == 'little':
+        x = np.fliplr(x)
+    x = np.pad(x, ((0, 0), (8 - nbits, 0)), 'constant')  # padding required to use packbits
+    if bitorder == 'little':
+        x = np.fliplr(x)
+    return np.packbits(x, bitorder=bitorder, axis=1)
 
 # Utility functions to mimic MATLAB's int2bit/bit2int
+
